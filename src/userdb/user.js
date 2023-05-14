0219d5912fb66c3ab5aa068e6149cb640b3df6e3
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-05-14 21:24:35 +0530

    New

--- src/userdb/user.js
+++ src/userdb/user.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
 
 const userSchema=new mongoose.Schema({
     fullname:{
@@ -21,6 +22,24 @@ const userSchema=new mongoose.Schema({
         unique:true
     }
 })
+
+userSchema.pre('save', async function(next) {
+    try {
+        // Generate a salt to use with bcrypt
+        const salt = await bcrypt.genSalt(10);
+
+        // Hash the password with the salt
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+
+        // Replace the plaintext password with the hashed password
+        this.password = hashedPassword;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 const users_collection=new mongoose.model('user_collection1',userSchema);
 module.exports=users_collection;
